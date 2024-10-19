e9fdcbb9fd101fcd2e3b59f65b16d5ce32601e36
Kaelin <40650381@live.napier.ac.uk>
2024-10-19 18:11:22 +0100

    Updated the report classes

--- src/main/java/com/napier/sem/CountryReport.java
+++ src/main/java/com/napier/sem/CountryReport.java
@@ -1,4 +1,37 @@
 package com.napier.sem;
 
-public class CountryReport {
+/**
+ * Class for the Country Report
+ */
+public class CountryReport
+{
+    /**
+     * Country Code
+     */
+    public String code;
+
+    /**
+     * Country Name
+     */
+    public String name;
+
+    /**
+     * Country Continent
+     */
+    public String continent;
+
+    /**
+     * Country Region
+     */
+    public String region;
+
+    /**
+     * Country Population
+     */
+    public int population;
+
+    /**
+     * Country Capital
+     */
+    public String capital;
 }
--- src/main/java/com/napier/sem/PopulationReport.java
+++ src/main/java/com/napier/sem/PopulationReport.java
@@ -1,4 +1,24 @@
 package com.napier.sem;
 
-public class PopulationReport {
+public class PopulationReport
+{
+    /**
+     * Name of the Continent/region/country
+     */
+    public String name;
+
+    /**
+     * total population of the continent/region/country
+     */
+    public int population;
+
+    /**
+     * total population of the continent/region/country living in a city(including the percentage)
+     */
+    public float populationInCity;
+
+    /**
+     * total population of the continent/region/country not living in a city(including the percentage)
+     */
+    public float populationNotInCity;
 }

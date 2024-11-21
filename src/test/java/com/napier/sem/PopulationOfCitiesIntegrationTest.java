bc077697c320a2e4f323109b072617984525dd04
Kaelin <40650381@live.napier.ac.uk>
2024-11-21 19:24:13 +0000

    renamed PopulationOfCitiesIntegrationTest

--- src/test/java/com/napier/sem/PopulationOfCitiesIntegrationTest.java
+++ src/test/java/com/napier/sem/PopulationOfCitiesIntegrationTest.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PopulationOfCitiesIntergationTest {
+public class PopulationOfCitiesIntegrationTest {
     static PopulationOfCities cities;
     static App app;
 
